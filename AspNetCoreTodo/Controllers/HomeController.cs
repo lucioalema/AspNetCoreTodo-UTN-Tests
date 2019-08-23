dc5c2c7777d13e309fab324411c44906f1308f38
Lema Lucio (Contractor) <LEMA@kapsch.net>
2019-08-23 17:58:23 -0300

    Initial commit

---
+++ AspNetCoreTodo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            throw new Exception("Error en Accion Privacy");
+            //return View();
+        }
+
+        [ResponseCache(Duration = 0, 
+        Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = 
+                HttpContext.Features
+                .Get<IExceptionHandlerPathFeature>();
+
+            logger.LogError($"La ruta {exceptionHandlerPathFeature.Path} " +
+            $"lanzo una excepcion {exceptionHandlerPathFeature.Error}");
+
+            return View(new ErrorViewModel { 
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                Path = exceptionHandlerPathFeature.Path
+            });
+        }
+    }
+}
---
+++ AspNetCoreTodo/Services/FakeTodoService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "ASP.NET Core - MVC",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+            var item2 = new TodoItem
+            {
+                Title = "ASP.NET Core - Web Api",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+            var item3 = new TodoItem
+            {
+                Title = "React",
+                DueAt = DateTimeOffset.Now.AddMonths(2)
+            };
+            return Task.FromResult(new[] { item1, item2, item3 });
+        }
+
+        public Task<bool> MarkDoneAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
